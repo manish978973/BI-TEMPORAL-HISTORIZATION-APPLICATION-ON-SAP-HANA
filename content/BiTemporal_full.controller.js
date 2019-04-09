53477e8d594c52bc6144043da64fb5cd598f53a0
manish978973 <manishrchandran@gmail.com>
2019-04-09 13:36:32 +0200

    Latest commit....

---
+++ content/BiTemporal_full.controller.js
@@ -0,0 +1,86 @@
+sap.ui.controller("content.BiTemporal_full", {
+// controller logic goes here
+onInit: function() {
+    
+    // first, we have to define the odata model
+var dataModel = new sap.ui.model.odata.ODataModel(
+"models/GBI.xsodata"
+);
+// after that, we can bind the odata model the
+// SalesOrders view, so controls within the view can use them
+this.getView().setModel(dataModel);
+// this function is called when the view is instantiated.
+// Used to modify the view before displaying
+},
+onExit: function() {
+// this function is called when the view is destroyed.
+// Used for clean-up activities
+},
+onAfterRendering: function() {
+// this function is called when the view has been rendered.
+// Used for post-rendering manipulation of the HTML code
+},
+onBeforeRendering: function() {
+// this function is called before the view is re-rendered // (not before first rendering)
+},
+
+onFilter1Invoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+			//	aFilter.push(new sap.ui.model.Filter("CUSTOMER_NAME", sap.ui.model.FilterOperator.Contains, sQuery));
+				aFilter.push(new sap.ui.model.Filter("CITY", sap.ui.model.FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.getView().byId("table");
+			var oBinding = oList.getBinding("rows");
+			oBinding.filter(aFilter);
+		},
+		
+onFilter2Invoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+			//	aFilter.push(new sap.ui.model.Filter("CUSTOMER_NAME", sap.ui.model.FilterOperator.Contains, sQuery));
+				aFilter.push(new sap.ui.model.Filter("CUSTOMER_NUMBER", sap.ui.model.FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.getView().byId("table");
+			var oBinding = oList.getBinding("rows");
+			oBinding.filter(aFilter);
+		},
+		
+		
+onFilter3Invoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new sap.ui.model.Filter("CUSTOMER_NAME", sap.ui.model.FilterOperator.Contains, sQuery));
+			//	aFilter.push(new sap.ui.model.Filter("CITY", sap.ui.model.FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.getView().byId("table");
+			var oBinding = oList.getBinding("rows");
+			oBinding.filter(aFilter);
+		},
+// this function updates the details panel
+	clearAllFilters : function() {
+			var oTable = this.byId("table");
+
+
+			var aColumns = oTable.getColumns();
+			for (var i = 0; i < aColumns.length; i++) {
+				oTable.filter(aColumns[i], null);
+			}
+		}
+
+});
---
+++ content/Home.controller.js
@@ -0,0 +1,26 @@
+sap.ui.controller("content.Home", {
+
+	// controller logic goes here
+    
+    onInit: function() {
+    	// this function is called when the view is instantiated. 
+
+        
+    },
+
+		
+    onExit: function() {
+    	// this function is called when the view is destroyed. 
+    	// Used for clean-up activities
+    },
+    
+    onAfterRendering: function() {
+    	// this function is called when the view has been rendered. 
+    	// Used for post-rendering manipulation of the HTML code
+    },
+    
+    onBeforeRendering: function() {
+    	// this function is called before the view is re-rendered 
+    	// (not before first rendering)
+    }
+});
